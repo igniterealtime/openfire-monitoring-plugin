86563a4b7636164c4fef4413d9b8d95458e42780
Guus der Kinderen <guus@goodbytes.nl>
2015-12-07 15:31:35 +0100

    OF-993: Replace thread factory boilerplate code.

--- src/java/org/jivesoftware/openfire/reporting/util/TaskEngine.java
+++ src/java/org/jivesoftware/openfire/reporting/util/TaskEngine.java
@@ -1,4 +1,7 @@
-/*
+/**
+ * $Revision$
+ * $Date$
+ *
  * Copyright (C) 2008 Jive Software. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -303,7 +306,7 @@ public class TaskEngine implements Disposable {
         }
 
         @Override
-        public void run() {
+		public void run() {
             executor.submit(task);
         }
     }
