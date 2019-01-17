f1adbc682f5669e499490b1e805434d8ca3b8f23
Guus der Kinderen <guus.der.kinderen@gmail.com>
2019-01-18 00:24:52 +0100

    Add support for MAM:2 (MUC only)
    Support for MAM:2 is possible from Openfire 4.3.1 onwards, assuming that
    https://github.com/igniterealtime/Openfire/pull/1265 makes it in that version.
    
    This partially fixes https://github.com/igniterealtime/openfire-monitoring-plugin/issues/5
    (partially, as this commit adds support only for MUC).

---
+++ src/java/com/reucon/openfire/plugin/archive/xep0313/IQQueryHandler2.java
@@ -0,0 +1,41 @@
+package com.reucon.openfire.plugin.archive.xep0313;
+
+import org.dom4j.Element;
+import org.jivesoftware.openfire.XMPPServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xmpp.packet.IQ;
+import org.xmpp.packet.JID;
+
+/**
+ * XEP-0313 IQ Query Handler
+ */
+class IQQueryHandler2 extends IQQueryHandler {
+
+    private static final Logger Log = LoggerFactory.getLogger( IQQueryHandler2.class);
+    private static final String MODULE_NAME = "Message Archive Management Query Handler v2";
+    private static final String domain = XMPPServer.getInstance().getServerInfo().getXMPPDomain();
+
+    IQQueryHandler2() {
+        super(MODULE_NAME, "urn:xmpp:mam:2");
+    }
+
+    @Override
+    protected void sendEndQuery(IQ packet, JID jid, QueryRequest queryRequest) {
+        sendAcknowledgementResult(packet, jid, queryRequest);
+    }
+
+    /**
+     * Send result packet to client acknowledging query.
+     * @param packet Received query packet
+     * @param JID to respond to
+     */
+    private void sendAcknowledgementResult(IQ packet, JID from, QueryRequest queryRequest) {
+        if (packet.getTo() == null) packet.setTo(domain);
+
+        IQ result = IQ.createResultIQ(packet);
+        Element fin = result.setChildElement("fin", NAMESPACE);
+        completeFinElement(queryRequest, fin);
+        router.route(result);
+    }
+}
