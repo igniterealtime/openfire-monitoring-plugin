c7edae3301129c05264c0cfa521524bd4160a6e4
Guus der Kinderen <guus.der.kinderen@gmail.com>
2019-01-18 00:24:52 +0100

    Add support for MAM:2 (MUC only)
    Support for MAM:2 is possible from Openfire 4.3.1 onwards, assuming that
    https://github.com/igniterealtime/Openfire/pull/1265 makes it in that version.
    
    This partially fixes https://github.com/igniterealtime/openfire-monitoring-plugin/issues/5
    (partially, as this commit adds support only for MUC).

---
+++ src/java/com/reucon/openfire/plugin/archive/xep0313/Xep0313Support2.java
@@ -0,0 +1,23 @@
+package com.reucon.openfire.plugin.archive.xep0313;
+
+import com.reucon.openfire.plugin.archive.xep.AbstractXepSupport;
+import org.jivesoftware.openfire.XMPPServer;
+
+import java.util.ArrayList;
+
+/**
+ * Encapsulates support for <a
+ * href="http://www.xmpp.org/extensions/xep-0313.html">XEP-0313</a>.
+ */
+public class Xep0313Support2 extends AbstractXepSupport {
+
+    private static final String NAMESPACE = "urn:xmpp:mam:2";
+
+    public Xep0313Support2( XMPPServer server) {
+        super(server, NAMESPACE,NAMESPACE, "XEP-0313 IQ Dispatcher", true);
+
+        this.iqHandlers = new ArrayList<>();
+        iqHandlers.add(new IQQueryHandler2());
+    }
+
+}
