90f4f2f4cd8de7b4c1ef567a734f70e65b9ef0fc
Guus der Kinderen <guus@goodbytes.nl>
2020-01-28 14:15:57 +0100

    fixes #35: Add webpage to access MUC logs
    This commit adds a webpage that uses the API that was added earlier to allow uses to view groupchat logs in their browser.

--- src/java/org/jivesoftware/openfire/plugin/service/LogAPI.java
+++ src/java/org/jivesoftware/openfire/plugin/service/LogAPI.java
@@ -35,7 +35,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Path("api/logs")
+@Path("logs")
 @Produces( MediaType.APPLICATION_JSON)
 public class LogAPI
 {
