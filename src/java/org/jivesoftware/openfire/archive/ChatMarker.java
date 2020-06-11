7acaed3aa22f3d1745d2f37f204f0c10e0c78ecb
mightymop <totzkotz@gmail.com>
2020-06-11 08:10:20 +0200

    add brackets

--- src/java/org/jivesoftware/openfire/archive/ChatMarker.java
+++ src/java/org/jivesoftware/openfire/archive/ChatMarker.java
@@ -14,7 +14,9 @@ public class ChatMarker {
     public static TYPE searchForXep0333(String stanza)
     {
         if (stanza==null)
+        {
             return TYPE.NONE;
+        }
         
         int idxmarkable = stanza.indexOf("<markable");
         int idxreceived= stanza.indexOf("<received");
@@ -25,7 +27,9 @@ public class ChatMarker {
         {
             int idxEnd = stanza.indexOf("/>",idxmarkable);
             if (idxEnd==-1)
+            {
                 idxEnd = stanza.indexOf("</markable>",idxmarkable);
+            }
             return idxEnd!=-1?(stanza.substring(idxmarkable, idxEnd).contains("urn:xmpp:chat-markers:0")?TYPE.MARKABLE:TYPE.NONE):TYPE.NONE;
         }
         else
@@ -33,7 +37,9 @@ public class ChatMarker {
             {
                 int idxEnd = stanza.indexOf("/>",idxreceived);
                 if (idxEnd==-1)
+                {
                     idxEnd = stanza.indexOf("</received>",idxmarkable);
+                }
                 return idxEnd!=-1?(stanza.substring(idxreceived, idxEnd).contains("urn:xmpp:chat-markers:0")?TYPE.RECEIVED:TYPE.NONE):TYPE.NONE;
             }
             else 
@@ -41,7 +47,9 @@ public class ChatMarker {
                 {
                     int idxEnd = stanza.indexOf("/>",idxdisplayed);
                     if (idxEnd==-1)
+                    {
                         idxEnd = stanza.indexOf("</displayed>",idxmarkable);
+                    }
                     return idxEnd!=-1?(stanza.substring(idxdisplayed, idxEnd).contains("urn:xmpp:chat-markers:0")?TYPE.DISPLAYED:TYPE.NONE):TYPE.NONE;
                 }
                 else 
@@ -49,7 +57,9 @@ public class ChatMarker {
                     {
                         int idxEnd = stanza.indexOf("/>",idxacknowled);
                         if (idxEnd==-1)
+                        {
                             idxEnd = stanza.indexOf("</acknowledged>",idxmarkable);
+                        }
                         return idxEnd!=-1?(stanza.substring(idxacknowled, idxEnd).contains("urn:xmpp:chat-markers:0")?TYPE.ACKNOWLEGED:TYPE.NONE):TYPE.NONE;
                     }
                     else
