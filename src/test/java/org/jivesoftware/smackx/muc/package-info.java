d2e7c301e64e474294d8fd7a02e6c5de832073a2
Guus der Kinderen <guus.der.kinderen@gmail.com>
2025-11-28 14:54:34 +0100

    CI: run (custom) XMPP Interop Tests
    This adds integration tests that are part of our codebase, and lets them run using the XMPP Interop Test framework.

---
+++ src/test/java/org/jivesoftware/smackx/muc/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Integration Tests that are intended to be executed by the XMPP Interop Testing framework.
+ *
+ * @see <a href="https://xmpp-interop-testing.github.io/">XMPP Interop Testing framework</a>
+ */
+package org.jivesoftware.smackx.muc;
